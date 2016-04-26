2a25b233406c22ab03d6f78cbea3c828e64ae204
elbereth85 <elbereth.earendil@gmail.com>
2016-04-26 15:36:16 +0200

    Added files via upload
    V2.0

--- Kata1/Kata1/PPTLS.cs
+++ Kata1/Kata1/PPTLS.cs
@@ -1,18 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Kata1
+﻿namespace Kata1
 {
     public class Jugada
     {       
+        /// <summary>
+        /// Devuelve Gana, Pierde o Empate respecto a la primera jugada (Jugador1)
+        /// Si alguna de las dos jugadas fuera nula o de otro tipo, devolvería inválido
+        /// </summary>
+        /// <param name="jugada1"></param>
+        /// <param name="jugada2"></param>
+        /// <returns>Enum</returns>
         public Resultado Play(Gesto jugada1, Gesto jugada2)
         {
+
             if (jugada1.GetType() != typeof(Gesto) || jugada2.GetType() != typeof(Gesto)) return Resultado.Invalido;
 
-            if (jugada1.Equals(jugada2)) return Resultado.Empate; 
+            if (jugada1.Equals(jugada2)) return Resultado.Empate;
 
             if (jugada1.Equals(Gesto.Piedra) && (jugada2.Equals(Gesto.Tijera) || jugada2.Equals(Gesto.Lagarto))) return Resultado.Gana;
 
